c38431eb4a7089d1d821cc509bc3c850e4c90308
MaksimSokolovVL <volokos2010@gmail.com>
2023-03-06 11:40:16 +0300

    init

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,6 +2,7 @@ package app;
 
 import app.config.AppConfig;
 import app.model.AnimalsCage;
+import app.model.Timer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -13,8 +14,11 @@ public class Application {
         for (int i = 0; i < 5; i++) {
             AnimalsCage bean =
                     applicationContext.getBean(AnimalsCage.class);
+            System.out.println(bean.getTimer().getTime().longValue());
             bean.whatAnimalSay();
         }
     }
+
+
 
 }
